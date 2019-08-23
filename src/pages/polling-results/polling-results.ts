f0e625817e1cc5f89af58934d4296cbcf5f8e3b9
arulyan <41385173+arulyan@users.noreply.github.com>
2019-08-24 00:05:34 +0530

    Added Graphs

--- src/pages/polling-results/polling-results.ts
+++ src/pages/polling-results/polling-results.ts
@@ -1,7 +1,7 @@
 import { ServiceProvider } from './../../providers/service/service';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Chart } from "chart.js";
 /**
  * Generated class for the PollingResultsPage page.
  *
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'polling-results.html',
 })
 export class PollingResultsPage {
+  @ViewChild("barCanvas") barCanvas: ElementRef;
+  public barChart: Chart;
   event: any;
   data: any;
   a: number = 0;
@@ -28,8 +30,9 @@ export class PollingResultsPage {
     this.getVotingResults();
   }
 
-  ionViewDidLoad() {
+  ionViewDidEnter() {
     console.log('ionViewDidLoad PollingResultsPage');
+    this.generateGraph();
   }
 
   getVotingResults() {
@@ -58,6 +61,45 @@ export class PollingResultsPage {
       console.log(this.d);
       console.log(res.json());
     })
+  }
+
+  generateGraph(){
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: [`${this.event.OptionA}`, `${this.event.OptionB}`,`${this.event.OptionC}`, `${this.event.OptionD}`],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [this.a, this.b, this.c, this.d],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
   }
 
 }
